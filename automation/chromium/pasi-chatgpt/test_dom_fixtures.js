abedc54ab37f3938dc9717a1c361865699b9e58e
bread <bergriley551@gmail.com>
2026-09-19 20:38:28 -0500

    test(chromium): make multiline mutation fixture deterministic

--- automation/chromium/pasi-chatgpt/test_dom_fixtures.js
+++ automation/chromium/pasi-chatgpt/test_dom_fixtures.js
@@ -28,10 +28,11 @@ function loadController(html, { mutateWhitespace = false } = {}) {
   dom.window.eval(DETECTORS);
   let source = CONTENT;
   if (mutateWhitespace) {
-    source = source.replace(
-      ".replace(/\\r\\n?/g, '\\n')\n      .replace(/[ \\t]+(?=\\n)/g, '')",
-      ".replace(/\\s+/g, ' ')"
-    );
+    const mutationPattern =
+      /\\.replace\\(\\/\\\\r\\\\n\\?\\/g, '\\\\n'\\)\\s+\\.replace\\(\\/\\[ \\t\\]\\+\\(\\?=\\\\n\\)\\/g, ''\\)/;
+    const mutated = source.replace(mutationPattern, ".replace(/\\s+/g, ' ')");
+    assert.notEqual(mutated, source, 'whitespace-collapse mutation did not match messageText');
+    source = mutated;
   }
   dom.window.eval(source);
   assert.ok(dom.window.PASI_NATIVE_TEST_API);
